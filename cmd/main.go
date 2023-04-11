d4ad4249da1a98a9d8c80e3c75cdc8940bae5bda
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2023-04-11 23:11:22 +0000

    base functionality

---
+++ cmd/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Item struct {
+	Title		string `xml:"title"`
+	Description	string `xml:"description"`
+	Link		string `xml:"link"`
+}
+
+type Channel struct {
+	Title 	string `xml:"title"`
+	Description string `xml:"description"`
+	Link string `xml:"link"`
+	Items []Item `xml:"item"`
+}
+
+type RSS struct {
+	XMLName xml.Name `xml:"rss"`
+	Channel Channel `xml:"channel"`
+}
+
+func main() {
+	fmt.Println("Hacker News")
+
+	// Fetch RSS feed
+	resp, err := http.Get("https://feeds.feedburner.com/TheHackersNews")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	// Parse RSS feed
+	var rss RSS
+	err = xml.NewDecoder(resp.Body).Decode(&rss)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Display news items in console
+	fmt.Println("Latest news:")
+	for _, item := range rss.Channel.Items {
+		fmt.Println("Title:", item.Title)
+		fmt.Println("Description:", item.Description)
+		fmt.Println("Link:", item.Link)
+		fmt.Println()
+	}
+
+}
